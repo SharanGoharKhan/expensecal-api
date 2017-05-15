9857dac24e4ca721ca0466629363e340fa3ad3d3
Sharan Khan <sharan.gohar@gmail.com>
2017-05-16 00:35:14 +0500

    added seed for categories

---
+++ sql/seed.sql
@@ -0,0 +1,6 @@
+source sql/schema.sql
+INSERT INTO `category` (`id`, `name`, `deleted_at`, `updated_at`, `created_at`) VALUES (NULL, 'Food expenses', NULL, NULL, CURRENT_TIMESTAMP);
+INSERT INTO `category` (`id`, `name`, `deleted_at`, `updated_at`, `created_at`) VALUES (NULL, 'Living expenses', NULL, NULL, CURRENT_TIMESTAMP);
+INSERT INTO `category` (`id`, `name`, `deleted_at`, `updated_at`, `created_at`) VALUES (NULL, 'Miscellaneous expenses', NULL, NULL, CURRENT_TIMESTAMP);
+INSERT INTO `category` (`id`, `name`, `deleted_at`, `updated_at`, `created_at`) VALUES (NULL, 'Car Expenses', NULL, NULL, CURRENT_TIMESTAMP);
+INSERT INTO `category` (`id`, `name`, `deleted_at`, `updated_at`, `created_at`) VALUES (NULL, 'Trip expenses', NULL, NULL, CURRENT_TIMESTAMP);
